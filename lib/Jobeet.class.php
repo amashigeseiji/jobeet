19a605dcd2ba86084c4dcee3c602a34ea0dd406b
amashigeseiji <amashige@tejimaya.com>
2014-06-02 00:45:56 +0900

    Jobeet::slugfy()メソッドを修正(8th days)

--- lib/Jobeet.class.php
+++ lib/Jobeet.class.php
@@ -4,9 +4,27 @@ class Jobeet
   static public function slugfy($text)
   {
     // 文字ではないもしくは数値ではないものすべてを - に置き換える
-    $text = preg_replace('/\W+/', '-', $text);
-    //トリムして小文字に変換
-    $text = strtolower(trim($text, '-'));
+    $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+
+    //トリム
+    $text = trim($text, '-');
+
+    //翻字する
+    if (function_exists('iconv'))
+    {
+      $text = iconv('utf8', 'us-ascii//TRANSLIT', $text);
+    }
+
+    //小文字に変換
+    $text = strtolower($text);
+
+    //望まない文字を取り除く
+    $text = preg_replace('#[^-\w]+#', '', $text);
+
+    if (empty($text))
+    {
+      $text = 'n-a';
+    }
 
     return $text;
   }
