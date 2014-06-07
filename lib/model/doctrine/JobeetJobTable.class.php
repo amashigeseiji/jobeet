e24fd416a1e93332c7ea6a23d722e707843064d9
amashigeseiji <amashige@tejimaya.com>
2014-06-07 13:18:45 +0900

    選択可能なtypeを設定

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -7,6 +7,17 @@
  */
 class JobeetJobTable extends Doctrine_Table
 {
+  static public $types = array(
+    'full-time' => 'Full time',
+    'part-time' => 'Part time',
+    'freelance' => 'Freelance',
+  );
+
+  public function getTypes()
+  {
+    return self::$types;
+  }
+
   /**
    * Returns an instance of this class.
    *
